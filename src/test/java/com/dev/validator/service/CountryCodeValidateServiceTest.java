c14d301f05aba1d98b0632a0bf1f5cb3a354669f
devel <devel.igr@gmail.com>
2023-08-07 06:26:46 +0300

    Добавление тестов

--- src/test/java/com/dev/validator/service/CountryCodeValidateServiceTest.java
+++ src/test/java/com/dev/validator/service/CountryCodeValidateServiceTest.java
@@ -8,12 +8,16 @@ import com.dev.validator.dao.spring.CountryCodeRepository;
 import com.dev.validator.dao.spring.SpringDAOManager;
 import com.dev.validator.exception.ParseException;
 import com.dev.validator.exception.ValidateException;
+import com.dev.validator.exception.ValidateNotFoundException;
 import com.dev.validator.model.CountryCode;
 import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,6 +34,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.HttpClientErrorException;
 
 /**
  *
@@ -48,13 +53,11 @@ public class CountryCodeValidateServiceTest {
 //    @InjectMocks
     @Autowired
     private CountryCodeValidateService countryCodeValidateService;
-    
+
 //    @InjectMocks 
 //    private ParseService parseService;
-    
 //    @Autowired
 //    TestEntityManager testEntityManager;
-
 //    @BeforeAll
 //    public void initData() throws ParseException {
 //        parseService.parse().stream()
@@ -62,7 +65,6 @@ public class CountryCodeValidateServiceTest {
 //                .map(CountryCode.class::cast)
 //                .forEach(cc -> testEntityManager.persist(cc));
 //    }
-
     @Test
     @DisplayName("testBahamas")
     public void testBahamas() throws ValidateException {
@@ -74,7 +76,7 @@ public class CountryCodeValidateServiceTest {
     @DisplayName("testUSACanada")
     public void testUSACanada() throws ValidateException {
         List<CountryCode> codes = countryCodeValidateService.validate("11165384765");
-        assertThat(codes).extracting(CountryCode::getCountry).contains("United States","Canada");
+        assertThat(codes).extracting(CountryCode::getCountry).contains("United States", "Canada");
     }
 
     @Test
@@ -91,6 +93,42 @@ public class CountryCodeValidateServiceTest {
     public void testKazakhstan() throws ValidateException {
         List<CountryCode> codes = countryCodeValidateService.validate("77112227231");
         assertThat(codes).extracting(CountryCode::getCountry).contains("Kazakhstan");
+    }
+
+    @Test
+    @DisplayName("testNotFound")
+    public void testNotFound() throws ValidateException {
+        ValidateNotFoundException thrown = assertThrows(
+                ValidateNotFoundException.class,
+                () -> countryCodeValidateService.validate("000000000000"),
+                ValidateNotFoundException.COUNTRY_NOT_FOUND
+        );
+//        Assertions.assertEquals(ValidateNotFoundException.COUNTRY_NOT_FOUND, thrown.getMessage());
+        assertTrue(thrown.getMessage().contains(ValidateNotFoundException.COUNTRY_NOT_FOUND));
+
+    }
+    
+    
+    @Test
+    @DisplayName("testLiteralCode")
+    public void testLiteralCode() throws ValidateException {
+        ValidateNotFoundException thrown = assertThrows(
+                ValidateNotFoundException.class,
+                () -> countryCodeValidateService.validate("ssadsadsa"),
+                ValidateNotFoundException.COUNTRY_NOT_FOUND
+        );
+//        Assertions.assertEquals(ValidateNotFoundException.COUNTRY_NOT_FOUND, thrown.getMessage());
+        assertTrue(thrown.getMessage().contains(ValidateNotFoundException.COUNTRY_NOT_FOUND));
+
+    }
+    
+    @Test
+    @DisplayName("testPlus")
+    public void testPlus() throws ValidateException {
+        List<CountryCode> codes = countryCodeValidateService.validate("+71423423412");
+        assertThat(codes).extracting(CountryCode::getCountry).contains("Russia");
+//        assertEquals(codes.stream().map(CountryCode::getCountry).collect(Collectors.joining(",")), "Russia");
+
     }
 
 }
